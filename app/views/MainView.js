fa2c094a433841ad4130dabd4cce9f93a70e59f7
rijkvanzanten <rijkvanzanten@me.com>
2017-10-05 13:40:08 -0400

    Implement ace editor

--- app/views/MainView.js
+++ app/views/MainView.js
@@ -1,13 +1,19 @@
-define([ 'app', 'backbone', 'core/t', 'core/extensions', 'core/notification', './CreateTemplateModalView', './EditTemplatePathModalView' ],
+require.config({
+  paths: {
+    ace: '../customs/extensions/static_generator/app/lib/ace'
+  }
+});
 
-function(app, Backbone, __t, Extension, Notification, CreateTemplateModalView, EditTemplatePathModalView ) {
+define([ 'app', 'backbone', 'core/t', 'core/extensions', 'core/notification', './CreateTemplateModalView', './EditTemplatePathModalView', 'ace/ace' ],
+
+function(app, Backbone, __t, Extension, Notification, CreateTemplateModalView, EditTemplatePathModalView, ace) {
 
 	return Extension.View.extend({
 	    template: 'static_generator/app/templates/main',
 	    initialize: function () {
 	      this.listenTo(this.collection.savedTemplates, 'sync', this.render);
-	      this.collection.savedTemplates.fetch();	
-	    }, 
+	      this.collection.savedTemplates.fetch();
+	    },
 	    serialize: function () {
 	      return {
 	        savedPages: this.collection.savedTemplates.pagesAsJSON(),
@@ -17,6 +23,14 @@ function(app, Backbone, __t, Extension, Notification, CreateTemplateModalView, E
 	        config: this.collection.savedTemplates.config()
 	      };
 	    },
+      afterRender: function() {
+	    	var tpl = this.collection.loadedTemplates.findWhere({selected: true});
+
+        if (tpl) {
+          this.editor = ace.edit('editor-'+tpl.get('id'));
+          this.editor.session.setMode('ace/mode/twig');
+        }
+      },
 	    events: {
 	        "click .create-new-template": "createTemplate",
 	        "click .page-route.save i": "saveTemplate",
@@ -25,12 +39,12 @@ function(app, Backbone, __t, Extension, Notification, CreateTemplateModalView, E
 	        "click .close-tab": "unloadTemplate",
 	        "click .tab, .file": "loadTemplate",
 	        "click #generate": "generateSite"
-	    },    
+	    },
 	    generateSite: function(e) {
 	    	var self = this,
 	    		outputDirectory = $('#output-dir').val(),
 	    		generationMethod = $('#generation').val();
-	    	
+
 			this.model.save({
 				'generationMethod': generationMethod,
 				'outputDirectory': outputDirectory
@@ -44,23 +58,23 @@ function(app, Backbone, __t, Extension, Notification, CreateTemplateModalView, E
 	    initSaveBtn: function(saveBtn) {
 	    	this.saveBtn = saveBtn;
 	    },
-	    loadTemplate: function(e){	    	
+	    loadTemplate: function(e){
 	    	var tpl = this.collection.loadedTemplates.findWhere({id: $(e.target).attr('data-id')}),
 				selected = this.collection.loadedTemplates.findWhere({selected: true});
 
 			if(selected) selected.set({selected:false});
-	    	
+
 	    	if( ! tpl) {
 	    		tpl = this.collection.savedTemplates.findWhere({id: $(e.target).attr('data-id')});
 	    		this.collection.loadedTemplates.push(tpl);
 	    	}
-	    	
+
 	    	this.saveBtn.setEnabled(true);
-	    	
-	    	tpl.set({selected:true}); 
+
+	    	tpl.set({selected:true});
 	    	this.render();
-	        
-	    },    
+
+	    },
 	    unloadTemplate: function(e) {
 	    	var tpl = this.collection.loadedTemplates.findWhere({id: $(e.target).attr('data-id')});
 
@@ -68,27 +82,27 @@ function(app, Backbone, __t, Extension, Notification, CreateTemplateModalView, E
 	    		this.collection.loadedTemplates.remove(tpl);
 	    		tpl = this.collection.loadedTemplates.first();
 	    		var selected = this.collection.loadedTemplates.findWhere({selected: true});
-	    		
-	    		if(!selected && tpl) tpl.set({selected:true}); 
-	    		
+
+	    		if(!selected && tpl) tpl.set({selected:true});
+
 	    		if( ! this.collection.loadedTemplates.length) {
 	    	    	this.saveBtn.setEnabled(false);
 	    		}
-	    		
+
 	    		this.render();
-	    	}    	
+	    	}
 	    },
 	    createTemplate: function(e) {
 	    	app.router.openViewInModal(new CreateTemplateModalView({
-	    		model: this.model	
-	    	}) );	        
+	    		model: this.model
+	    	}) );
 	    },
 	    editTemplatePath: function(e) {
 	    	var tpl = this.collection.savedTemplates.findWhere({id: $(e.target).attr('data-id')});
 
 	    	app.router.openViewInModal(new EditTemplatePathModalView({
 	    		model: tpl
-	    	}) );	        
+	    	}) );
 	    },
 	    saveTemplate: function(e) {
 	    	var tpl = this.collection.loadedTemplates.findWhere({selected: true});
@@ -96,27 +110,27 @@ function(app, Backbone, __t, Extension, Notification, CreateTemplateModalView, E
 
 			this.model.save({
 				'id': tpl.get('id'),
-				'contents': $('#loaded-template-content-'+tpl.get('id')).find('textarea.editor').val(),
+				'contents': this.editor.getValue(),
 				'filePath': tpl.get('file')
 			}, {
 				success : function(model, response, options) {
 					Notification.success(response.message);
-					Backbone.history.loadUrl(Backbone.history.fragment); // TODO: need to update models, 
-																		 // but don't really want to refresh 
+					Backbone.history.loadUrl(Backbone.history.fragment); // TODO: need to update models,
+																		 // but don't really want to refresh
 																		 // unsaved content in other tabs
 				}
-			});	        
+			});
 	    },
 	    deleteTemplate: function(e) {
 	    	var self = this;
 	        app.router.openModal({type: 'confirm', text: __t('Are you sure you want to delete this file?'), callback: function () {
-		    	var tpl = self.collection.savedTemplates.findWhere({id: $(e.target).attr('data-id')});	
+		    	var tpl = self.collection.savedTemplates.findWhere({id: $(e.target).attr('data-id')});
 
 		    	tpl.destroy({
 					success : function(model, response, options) {
 						Notification.success(response.message);
-						Backbone.history.loadUrl(Backbone.history.fragment); // TODO: need to update models, 
-																			 // but don't really want to refresh 
+						Backbone.history.loadUrl(Backbone.history.fragment); // TODO: need to update models,
+																			 // but don't really want to refresh
 																			 // unsaved content in other tabs
 					}
 				});
